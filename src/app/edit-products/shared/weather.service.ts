b502f189d620d97c0f44e985c2a31cefe8f81ea9
peterdillon <peter.dillon.biz@gmail.com>
2024-04-03 18:03:11 -0700

    update homepage

--- src/app/edit-products/shared/weather.service.ts
+++ src/app/edit-products/shared/weather.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class WeatherService {
 
   // Trial ended
-  url = '';
+  url = 'https://api.weatherapi.com/v1/current.json?key=5049bdea3fc840b89e611524241201&q=89012&aqi=no';
   // 'https://api.weatherapi.com/v1/current.json?key=5049bdea3fc840b89e611524241201&q=89012&aqi=no';
 
   constructor( private http: HttpClient ) { }
